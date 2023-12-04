24ae2a6b07c4c55650cfb4d4d8f1263b9daeebd3
AytacSafarova <aytaccseferr@gmail.com>
2023-12-04 12:00:55 +0400

    fix problems

---
+++ .history/pages/loginPage/index_20231204114119.js
@@ -0,0 +1,83 @@
+let form = document.querySelector("form");
+let label = document.querySelector(".label");
+let inputType = document.querySelector('input[type="password"]');
+let btn1 = document.querySelector(".btn-1");
+let btn2 = document.querySelector(".btn-2");
+let loginBtn = document.querySelector(".login");
+let email=document.querySelector('.email-input')
+let password=document.querySelector('.password')
+
+btn2.addEventListener("click", checkBtn2);
+function checkBtn2() {
+  inputType.type = "text";
+  btn2.style.display = "none";
+  btn1.style.display = "block";
+}
+
+btn1.addEventListener("click", checkBtn1);
+function checkBtn1() {
+  inputType.type = "password";
+  btn2.style.display = "block";
+  btn1.style.display = "none";
+}
+
+form.addEventListener("submit", submitForm);
+function submitForm(a) {
+  a.preventDefault();
+  // createUser()
+  getDatabase()
+ 
+}
+
+function getDatabase() {
+  let database= localStorage.getItem('users')
+  for (let index = 0; index < database.length; index++) {
+    if (database[index].email==email.value && database[index].password==password.value) {
+      console.log('var');
+
+    }
+
+    else{
+      console.log('yoxdur');
+    }
+   
+    
+  }
+  
+}
+
+
+
+
+// function createUser() {
+//     let user={
+//         id:Date.now(),
+//         email:email.value,
+//         password:inputType.value,
+//     };
+//     let database = getDatabase();
+//     database.push(user)
+//     localStorage.setItem('user',JSON.stringify(database))
+   
+// }
+
+
+// function getDatabase() {
+//     let database=JSON.parse(localStorage.getItem(database))
+//     if (database) {
+//         return database; 
+//     }
+//     else{
+//         createDatabase();
+//         let database=JSON.parse(localStorage.getItem(database))
+//         return database;
+//     }
+    
+// }
+
+// function createDatabase() {
+//     let usersArrey=[]
+//     localStorage.setItem('users',JSON.stringify(usersArrey))
+    
+// }
+
