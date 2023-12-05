81eb413ec9a9eefcdba739582f5aad47afa61c7f
AytacSafarova <aytaccseferr@gmail.com>
2023-12-05 12:35:06 +0400

    fix problems

---
+++ .history/pages/productPage/index_20231205121135.js
@@ -0,0 +1,62 @@
+let form=document.querySelector('form')
+let productImg=document.querySelector('.file-input')
+let productId=document.querySelector('.product-id-input')
+let productName=document.querySelector('.product-name-input')
+let productInfo=document.querySelector('.product-info-input')
+let productAmount=document.querySelector('.product-amount-input')
+let selectedCount =document.querySelector('.form-select')
+
+form.addEventListener('submit',submitForm)
+
+function submitForm(e) {
+    e.preventDefault()
+console.log(selectedCount.value);
+}
+
+
+// mehsulun yaradilmasi
+
+function createProduct() {
+    let product={
+        img:productImg.value,
+        id:productId.value,
+        name:productName.value,
+        amount:productAmount.value,
+        info:productInfo.value,
+        count:1
+
+    }
+
+    
+}
+
+
+// databasenin yaradilmasi
+
+function createDatabase() {
+    let productArrey=[]
+    localStorage.setItem('products',JSON.stringify(productArrey))
+
+    
+}
+
+
+// databasenin getirilmesi
+
+function getDatabase() {
+    let database=localStorage.getItem('products')
+    if (database) {
+        return JSON.parse(database)
+        
+    }
+
+    else{
+        createDatabase()
+        let database=localStorage.getItem('products')
+        return JSON.parse(database)
+    }
+    
+}
+
+
+
---
+++ .history/pages/productPage/index_20231205122309.js
@@ -0,0 +1,71 @@
+let form=document.querySelector('form')
+let productImg=document.querySelector('.file-input')
+let productId=document.querySelector('.product-id-input')
+let productName=document.querySelector('.product-name-input')
+let productInfo=document.querySelector('.product-info-input')
+let productAmount=document.querySelector('.product-amount-input')
+let selectedCount =document.querySelector('.form-select')
+form.addEventListener('submit',submitForm)
+
+function submitForm(e) {
+    e.preventDefault()
+checkProduct()
+}
+// mehsulun yoxlanmasi
+function checkProduct(){
+    let database = getDatabase()
+    for (let index = 0; index < database.length; index++) {
+        if(database[index].id== productId.value){
+            
+        }
+    }
+}
+
+// mehsulun yaradilmasi
+function createProduct() {
+    let product={
+        img:productImg.value,
+        id:productId.value,
+        name:productName.value,
+        amount:productAmount.value,
+        info:productInfo.value,
+        count:selectedCount.value
+
+    }
+    let database= getDatabase()
+    database.push(product)
+    localStorage.setItem('products',JSON.stringify(database))
+
+    
+}
+
+
+// databasenin yaradilmasi
+
+function createDatabase() {
+    let productArray=[]
+    localStorage.setItem('products',JSON.stringify(productArray))
+
+    
+}
+
+
+// databasenin getirilmesi
+
+function getDatabase() {
+    let database=localStorage.getItem('products')
+    if (database) {
+        return JSON.parse(database)
+        
+    }
+
+    else{
+        createDatabase()
+        let database=localStorage.getItem('products')
+        return JSON.parse(database)
+    }
+    
+}
+
+
+
